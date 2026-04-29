6e9145d47120952f1e8bf818ef561f7ff263bc66
xaionaro@dx.center <Dmitrii Okunev>
2026-04-29 15:33:31 +0000

    scripts/prod-ubuntu-chroot: exp backoff in loop-run-ffstream.sh (F5)

--- scripts/prod-ubuntu-chroot/loop-run-ffstream.sh
+++ scripts/prod-ubuntu-chroot/loop-run-ffstream.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-while sleep 0.1; do
+
+# Exponential backoff for ffstream supervisor.
+# - Tracks consecutive rapid-failure starts.
+# - Backs off 0.1 -> 1 -> 5 -> 30 seconds.
+# - Resets backoff after a successful long-running invocation (>= 60s).
+delay=0.1
+while true; do
+	start=$(date +%s)
 	run-ffstream.sh
+	end=$(date +%s)
+	dur=$((end - start))
+	if [ "$dur" -ge 60 ]; then
+		# Successful long run; reset backoff.
+		delay=0.1
+	else
+		# Rapid fail; escalate backoff.
+		case "$delay" in
+			0.1) delay=1 ;;
+			1)   delay=5 ;;
+			5)   delay=30 ;;
+			*)   delay=30 ;;  # cap
+		esac
+	fi
+	sleep "$delay"
 done
